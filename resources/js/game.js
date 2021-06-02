40105aa4f7c4a0620a187d9f0f6e0df87ed7e6fd
Kai Conkling <kaiconkling@gmail.com>
2021-06-01 21:50:18 -0700

    added button to test logic, changed JS location

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,20 +1,32 @@
+window.onload = init;
 
-function generateRockPaperScissors() {
+function init() {
+    console.log(getRockPaperOrScissors);
+}
+
+// document.getElementById("myButton").addEventListener('click', getRockPaperOrScissors());
+
+function getRockPaperOrScissors() {
 
     // Generate a randon number between 1-3
     const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
     switch(randomNumber) {
         case 1:
+            //console.log("Rock");
             return "Rock";
             break;
         case 2:
+            //console.log("Paper");
             return "Paper";
             break;
         case 3:
+            //console.log("Scissors");
             return "Scissors";
             break;
         default:
             return "Error";
     }
 }
+
+
